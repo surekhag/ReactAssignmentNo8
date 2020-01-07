ae329cf133dfc7f8ef6e153111e1453054e6ff03
surekhag <surekhag@github.com>
2020-01-07 18:20:31 +0530

    UI in progress

--- react-assignment/src/components/dashboard/UserInfo.js
+++ react-assignment/src/components/dashboard/UserInfo.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 // import  Container from '@material-ui/core';
-import {Container, Grid, TextField, Typography, ListItem} from '@material-ui/core';
+import {Container, Grid, TextField, Typography} from '@material-ui/core';
 import Title from './Title';
+import Select from 'react-select'
 
 function preventDefault(event) {
   event.preventDefault();
@@ -14,47 +15,58 @@ const useStyles = makeStyles({
     flex: 1,
   },
 });
+const ratingOptions = [
+    {value:11, label : 11 },
+    {value:12, label : 12 },
+    {value:13, label : 13 },
+    {value:14, label : 14 },
+    {value:15, label : 15 },
+    {value:16, label : 16 },
+    {value:17, label : 17 },
+    {value:18, label : 18 },
+    {value:19, label : 19 },
+    {value:20, label : 20 }
+];
+const keyResults = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+
+//   const MyComponent = () => (
+//     <Select options={options} />
+//   )
+  
 
 export default function Deposits() {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <Title>User Details</Title>
-      <Container>
-      {/* <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            // value={}
-            onChange={e => {
-              handleChange(e);
-              }}
-            autoComplete="current-password"
-          /> */}
- <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            Hi User
-          </Typography>
-          <div>Your role is</div>
+    <Title>User Details</Title>
+    <Container>      
+    <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+    Hi User
+    </Typography>
+    <div>Your role is</div>
 
 <form className={classes.root} noValidate autoComplete="off">
 <label for="my-input">Objective</label>  
-<TextField id="outlined-basic" label="Objective" variant="outlined" /> <br /> <br />
+<input id="outlined-basic" label="Objective" variant="outlined" /> <br /> <br />
 
 <label for="my-input">Weightage</label>
-<TextField id="outlined-basic" label="Weightage" variant="outlined" /><br /> <br />
-<ListItem /> 1
+<input id="outlined-basic" label="Weightage" variant="outlined" /><br /> <br />
+ Key Results : 
+ <Select options={ratingOptions}/>
 
-<label for="my-input">Rating</label>
-<TextField id="outlined-basic" label="Weightage" variant="outlined" /><br /> <br />
-
-<label for="my-input">Comments</label>
-<TextField id="outlined-basic" label="Weightage" variant="outlined" /><br /> <br />
-
+ Rating : 
+ {/* <Select  options={ratingOptions}/> */}
+{/* <Select
+        // value={selectedOption}
+        // onChange={this.handleChange}
+        options={options}
+      /> */}
+Comments : 
+<textarea></textarea>
 </form>
          
     </Container>
